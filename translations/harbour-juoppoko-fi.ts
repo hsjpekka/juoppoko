dfa3d052352eac63a83137b5537451f4d3899dd5
hsjpekka <pekka.marjamaki@iki.fi>
2017-02-16 23:08:05 +0200

    kieli vaihdettu

---
+++ translations/harbour-juoppoko-fi.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>Kansi</name>
+    <message>
+        <source> ‰ at </source>
+        <translation> ‰ klo </translation>
+    </message>
+</context>
+<context>
+    <name>Paaikkuna</name>
+    <message>
+        <source>delete</source>
+        <translation>poista</translation>
+    </message>
+    <message>
+        <source>deleting</source>
+        <translation>poistaa</translation>
+    </message>
+    <message>
+        <source>modify</source>
+        <translation>muokkaa</translation>
+    </message>
+    <message>
+        <source>info</source>
+        <translation>tietoja</translation>
+    </message>
+    <message>
+        <source>settings</source>
+        <translation>asetukset</translation>
+    </message>
+    <message>
+        <source>Drunkard?</source>
+        <translation>Juoppoko</translation>
+    </message>
+    <message>
+        <source>BAC</source>
+        <translation>veressä</translation>
+    </message>
+    <message>
+        <source>sober at</source>
+        <translation>selvänä</translation>
+    </message>
+    <message>
+        <source> ‰ at</source>
+        <translation> ‰ klo</translation>
+    </message>
+    <message>
+        <source>beer</source>
+        <translation>olut</translation>
+    </message>
+    <message>
+        <source>vol-%</source>
+        <translation>til-%</translation>
+    </message>
+    <message>
+        <source>time</source>
+        <translation>aika</translation>
+    </message>
+    <message>
+        <source>drink</source>
+        <translation>juoma</translation>
+    </message>
+    <message>
+        <source>cheers!</source>
+        <translation>skåål!</translation>
+    </message>
+    <message>
+        <source>wk</source>
+        <translation>vk</translation>
+    </message>
+</context>
+<context>
+    <name>asetukset</name>
+    <message>
+        <source>info</source>
+        <translation>tietoja</translation>
+    </message>
+    <message>
+        <source>restore</source>
+        <translation>palauta</translation>
+    </message>
+    <message>
+        <source>Data</source>
+        <translation>Arvot</translation>
+    </message>
+    <message>
+        <source>weight</source>
+        <translation>paino</translation>
+    </message>
+    <message>
+        <source>lever %</source>
+        <translation>maksa %</translation>
+    </message>
+    <message>
+        <source>100% - healthy</source>
+        <translation>100% - terve</translation>
+    </message>
+    <message>
+        <source>alcohol burning rate</source>
+        <translation>alkoholin palonopeus</translation>
+    </message>
+    <message>
+        <source>body water content</source>
+        <translation>kehon neste-%</translation>
+    </message>
+    <message>
+        <source>man</source>
+        <translation>mies</translation>
+    </message>
+    <message>
+        <source>woman</source>
+        <translation>nainen</translation>
+    </message>
+    <message>
+        <source>other</source>
+        <translation>muu</translation>
+    </message>
+    <message>
+        <source>calculate limits</source>
+        <translation>laske rajat</translation>
+    </message>
+    <message>
+        <source>lower limit</source>
+        <translation>alempi raja</translation>
+    </message>
+    <message>
+        <source>BAC - [‰]</source>
+        <translation>promillea</translation>
+    </message>
+    <message>
+        <source>upper limit</source>
+        <translation>ylempi raja</translation>
+    </message>
+    <message>
+        <source>lower weekly limit</source>
+        <translation>alempi viikkoraja</translation>
+    </message>
+    <message>
+        <source>alcohol [ml]</source>
+        <translation>alkoholia [ml]</translation>
+    </message>
+    <message>
+        <source>upper weekly limit</source>
+        <translation>ylempi viikkoraja</translation>
+    </message>
+    <message>
+        <source>lower yearly limit</source>
+        <translation>alempi vuosiraja</translation>
+    </message>
+    <message>
+        <source>upper yearly limit</source>
+        <translation>ylempi vuosiraja</translation>
+    </message>
+</context>
+<context>
+    <name>juomanMuokkaus</name>
+    <message>
+        <source>cocktail</source>
+        <translation>cocktail</translation>
+    </message>
+    <message>
+        <source>Drink</source>
+        <translation>Juoma</translation>
+    </message>
+    <message>
+        <source>-drinks-</source>
+        <translation>-juomia-</translation>
+    </message>
+    <message>
+        <source>beer</source>
+        <translation>olut</translation>
+    </message>
+    <message>
+        <source>wine</source>
+        <translation>viini</translation>
+    </message>
+    <message>
+        <source>schnaps</source>
+        <translation>paukku</translation>
+    </message>
+    <message>
+        <source>volume</source>
+        <translation>tilavuus</translation>
+    </message>
+    <message>
+        <source>alcohol</source>
+        <translation>prosentteja</translation>
+    </message>
+    <message>
+        <source>some notes?</source>
+        <translation>muuta?</translation>
+    </message>
+</context>
+<context>
+    <name>tietoja</name>
+    <message>
+        <source>The default limits are from </source>
+        <translation>Oletusrajat ovat sivulta </translation>
+    </message>
+    <message>
+        <source>according to which a safe limit for women is 7 portions weekly, for men 14. Limit of increased risk is 16 portions for women and 24 for men. Here the values are expected to depend only on the amount of water in body, not on sex.</source>
+        <translation>jonka mukaan kohtuukäytön raja on naisille 7 annosta viikossa ja miehille 14, ja riskiraja naisille 16 ja miehille 24 annosta. Arvojen oletetaan tässä ohjelmassa riippuvan vain kehon nesteen määrästä.</translation>
+    </message>
+    <message>
+        <translation>Alkoholin palamisnopeus samalta sivustolta.</translation>
+        <source>Alcohol burning rate is also from the same site.</source>
+    </message>
+</context>
+<context>
+    <name>tilastot</name>
+    <message>
+        <source>Statistics</source>
+        <translation>Tilastot</translation>
+    </message>
+    <message>
+        <source>last 24 h</source>
+        <translation>viimeiset 24 h</translation>
+    </message>
+    <message>
+        <source>last week</source>
+        <translation>viimeinen viikko</translation>
+    </message>
+    <message>
+        <source>, equals to </source>
+        <translation>, vastaa </translation>
+    </message>
+    <message>
+        <source>in year</source>
+        <translation>vuodessa</translation>
+    </message>
+    <message>
+        <source>last month</source>
+        <translation>viimeinen kuukausi</translation>
+    </message>
+    <message>
+        <source>in week</source>
+        <translation>viikossa</translation>
+    </message>
+</context>
+</TS>
